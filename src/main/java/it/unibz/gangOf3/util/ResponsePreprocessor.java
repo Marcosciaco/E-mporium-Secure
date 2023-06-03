aaa9c52f67bb2da81e111be945e8d8e5a7932c90
Elias Binder <ebinder@unibz.it>
2023-06-03 14:59:02 +0200

    Set correct response headers

--- src/main/java/it/unibz/gangOf3/util/ResponsePreprocessor.java
+++ src/main/java/it/unibz/gangOf3/util/ResponsePreprocessor.java
@@ -5,8 +5,9 @@ import jakarta.servlet.http.HttpServletResponse;
 public class ResponsePreprocessor {
 
     public static void preprocessResponse(HttpServletResponse response) {
-        response.addHeader("Access-Control-Allow-Origin", "*");
-        response.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        response.addHeader("Access-Control-Allow-Origin", "https://127.0.0.1:8443"); // Allow CORS
+        response.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS"); // Allow CORS
+        response.addHeader("X-FRAME-OPTIONS", "DENY"); // Prevent clickjacking
     }
 
 }
--- todo.md
+++ todo.md
@@ -8,7 +8,7 @@
 - [X] Use HTTPS
 - [X] Use UUIDs for the session token
 - [ ] Add password strength checker (for register and reset password)
-- [ ] Change CORS header and X-Frame-Options header
+- [X] Change CORS header and X-Frame-Options header
 - [ ] Add SQL injection protection
 - [ ] Add CSRF protection by using a token
 - [ ] Secure Rest API
@@ -17,3 +17,4 @@
 - [ ] Add timeout for login
 - [ ] Use password hashing
 - [ ] Encrypt chat messages
+- [ ] Check for security on all endpoints separately
